7793a77aa43f09165c54db379d23c97d53b11683
Ruben Taylor <65642058+rubentaylor@users.noreply.github.com>
2024-06-02 19:34:34 -0500

    Implemented important CPU specifications

---
+++ Chip-8.h
@@ -0,0 +1,25 @@
+class Chip8 {
+
+private:
+    unsigned short opcode; // 2 bytes, 8 bits
+    unsigned char memory[4096]; // 4k memory
+
+    unsigned char R[16]; // Chip 8 has 16 8-bit registers
+    unsigned short I; // Index register
+    unsigned short pc; // Program counter
+
+    unsigned char gfx[64 * 32]; // pixel state / window
+
+    unsigned char delay_timer;
+    unsigned char sound_timer;
+
+public:
+    Chip8();
+    ~Chip8();
+
+    bool drawFlag;
+
+    void emulateCycle();
+    bool loadApplication(const char * filename);
+    void setKeys();
+};
